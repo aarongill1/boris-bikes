f12905e69a51ed3d2739ec6c1df15a1c1506b8e8
Dana Gajewska <mdgajewska@outlook.com>
2018-10-31 15:01:32 +0000

    Add attr_reader and dock method and tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,20 @@
 require 'Docking_Station'
 
 describe DockingStation do
-docking_station = DockingStation.new
+  docking_station = DockingStation.new
   it 'responds to method release_bike' do
-    expect(docking_station).to respond_to :release_bike
+    expect(subject).to respond_to :release_bike
   end
    it 'docking station releases bike' do
      bike = docking_station.release_bike
      expect(bike.working?).to equal(true)
-end
+   end
+   it 'docks the bike' do
+     bike = Bike.new
+     expect(subject.dock(bike)).to eq bike
+   end
+
+   it "responds to bike" do
+     expect(subject).to respond_to :bike
+   end
 end
