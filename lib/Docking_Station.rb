58db7f579e8c1adc0307f85c21e2aec54190541a
Dana Gajewska <mdgajewska@outlook.com>
2018-10-31 15:49:12 +0000

    Step 13 - Add exception if docking station is full

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -9,6 +9,7 @@ class DockingStation
   end
 
   def dock(bike)
+    fail 'Docking station unavailable' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,17 +10,20 @@ describe DockingStation do
      expect(subject.dock(bike)).to eq bike
    end
 
-   it "responds to bike" do
-     expect(subject).to respond_to :bike
-   end
    describe '#release_bike' do
      it 'docking station releases bike' do
        bike = Bike.new
        subject.dock(bike)
        expect(subject.release_bike).to eq bike
-   end
-    it 'raises and error when there are no bikes available' do
+     end
+     it 'raises and error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
- end
+     end
+     describe '#dock' do
+       it 'raises an error when the docking station is full' do
+         subject.dock(Bike.new)
+         expect { subject.dock Bike.new }.to raise_error 'Docking station unavailable'
+       end
+     end
 end
 end
