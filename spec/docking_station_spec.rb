033bfb6a283c7144928af9706c2fe9b2bfc1f7ae
Aaron Gill <izzydiggler@gmail.com>
2018-11-01 16:33:25 +0000

    challenge 18 completed - 3rd and last user story already completed and tested with previous commits. Refactored some tests to remove duplicate code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,14 @@
 require 'Docking_Station'
 
 describe DockingStation do
-   it 'docks the bike' do
-     bike = Bike.new
-     expect(subject.dock(bike)).to include bike
-   end
    it 'has specified capacity' do
       docking_station = DockingStation.new(1)
-      bike = Bike.new
-      expect(docking_station.dock(bike)).to include bike
+      docking_station.dock(Bike.new)
       expect{docking_station.dock(Bike.new)}.to raise_error 'Docking station unavailable'
    end
    it 'has no specified capacity and uses default value' do
-     docking_station = DockingStation.new
-     bike = Bike.new
-     DockingStation::DEFAULT_CAPACITY.times { docking_station.dock(Bike.new)}
-     expect{docking_station.dock(bike)}.to raise_error 'Docking station unavailable'
+     DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new)}
+     expect{subject.dock(Bike.new)}.to raise_error 'Docking station unavailable'
    end
 
    describe '#release_bike' do
